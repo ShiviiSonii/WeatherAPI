e0da330017d6ba36ca3bf76f418060c8e0eccb23
ShiviiSonii <112719064+ShiviiSonii@users.noreply.github.com>
2022-12-22 19:46:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+//Requiring Express Module
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+//Initialising Express Module
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+res.sendFile(__dirname+"/index.html");
+})
+
+app.get("/style.css",function(req,res){
+    res.sendFile(__dirname+"/style.css");
+});
+
+app.post("/",function(req,res){
+
+const query=req.body.cityName;
+    const apiKey="a6deb946081d1890d2cb6ff9dc089342";
+    const unit="metric";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + unit;
+   
+    https.get(url,function(response){
+        console.log(response);
+
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            const temp =weatherData.main.feels_like
+            const desc=weatherData.weather[0].description
+            const icon=weatherData.weather[0].icon
+            const imageURL= "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            
+            res.write("<p>The Weather is currently "+desc+"</p>");
+            res.write("<h1>The Temperature in " +query+ " is "+temp+" degrees celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send()
+        })
+    })
+})
+
+
+//Listening By a callback function
+app.listen(3000,function(req,res){
+console.log("Server is running on port 3000.");
+});
